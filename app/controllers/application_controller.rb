20559e1b650ab73d46e319034e93028d7e25b5f8
Daniel Lirio Reis <daniellirioreis@gmail.com>
2016-02-25 03:31:36 -0300

    Diagramação a acerto de responder na crud de exercicio

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ require "application_responder"
 class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :html
-
+  responders :flash
+ 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -16,5 +17,10 @@ class ApplicationController < ActionController::Base
 	end
 
   helper_method :message
+
+  def define_environment_message
+    message[:alert] ||= []
+  end
+
 
 end
