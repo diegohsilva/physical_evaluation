b9ac17257b951efa337a648f31c2da8dc5094876
diegohsilva <diegohsilva@gmail.com>
2016-03-03 09:21:13 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	config/locales/evaluation.yml
    #	db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301143059) do
-ActiveRecord::Schema.define(version: 20160302131255) do
 ActiveRecord::Schema.define(version: 20160302124024) do
 
   # These are extensions that must be enabled in order to support this database
