d2444a266badbd1fd89de3fd4577a77d0e5e1ee9
diegohsilva <diegohsilva@gmail.com>
2016-04-13 00:02:01 -0300

    ajustes no resultado
    Ajustes no resultado da avaliação

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -44,10 +44,111 @@ class Evaluation < ActiveRecord::Base
      q.round(2)
   end
   
+  def tmb
+    d = ccorp_altura * 100
+    age = student_age[:ano]
+    peso = ccorp_peso
+    l = 0.0
+    if student.male?
+       l = (66 + (13.7 * ccorp_peso) + (5.0 * d) - (6.8 * age))
+    else
+       l = (655 + (9.6 * ccorp_peso) + (1.8 * d) - (4.7 * age))
+    end
+    case amn_frequencia
+      when 0 
+         l = l * 1.2
+      when (1..2)
+         l = l * 1.375
+      when (3..4)     
+         l = l * 1.55
+      when (5..6)
+         l = l * 1.725
+      when (7..10) 
+         l = l * 1.9  
+    end     
+    l.to_i
+  end
 
-  
+  def media_gordura
+    age = student_age[:ano]
+    r = " "
+    if student.male?
+      if (18..25).include?(age)
+         r = "14,0 A 16,0%"
+      end   
+      if (26..35).include?(age)
+         r = "18,0 A 20,0%"
+      end   
+      if (36..45).include?(age)
+         r = "21,0 A 23,0%"
+      end               
+      if (46..55).include?(age)
+         r = "24,0 A 25,0%"
+      end               
+      if (56..65).include?(age)
+         r = "24,0 A 25,0%"
+      end               
+    else
+      if (18..25).include?(age)
+         r = "23,0 A 25,0%"
+      end   
+      if (26..35).include?(age)
+         r = "24,0 A 25,0%"
+      end   
+      if (36..45).include?(age)
+         r = "27,0 A 29,0%"
+      end               
+      if (46..55).include?(age)
+         r = "29,0 A 31,0%"
+      end               
+      if (56..65).include?(age)
+         r = "30,0 A 32,0%"
+      end               
+    end
+    r
+  end
+
+  def relacao_cq
+    age = student_age[:ano]
+    r = " "
+    if student.male?
+       if (15..29).include?(age)
+          r = "0,83 A 0,88  "
+       end
+       if (30..39).include?(age)
+          r = "0,84 A 0,91"
+       end
+       if (40..49).include?(age)
+          r = "0,88 A 0,95"
+       end
+       if (50..59).include?(age)
+          r = "0,90 A 0,96  "
+       end
+       if (60..69).include?(age)
+          r = "0,91 A 0,98  "
+       end
+    else
+       if (15..29).include?(age)
+          r = "0,71 A 0,77  "
+       end
+       if (30..39).include?(age)
+          r = "0,72 A 0,78  "
+       end
+       if (40..49).include?(age)
+          r = "0,73 A 0,79"
+       end
+       if (50..59).include?(age)
+          r = "0,74 A 0,81  "
+       end
+       if (60..69).include?(age)
+          r = "0,76 A 0,83    "
+       end
+    end   
+       r
+  end
+
   def classification_coporral_fat
-    c = {:position => nil, :text => nil}
+    c = {:position => 0, :text => nil}
 
      age = student_age[:ano]
      g = gordura
@@ -128,6 +229,7 @@ class Evaluation < ActiveRecord::Base
       else
               
       end
+
       c
   end
   
